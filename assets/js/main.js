872ed0e152eaa85bac75cb866243109135db915a
rika <reganari48@gmail.com>
2025-06-03 14:28:21 +0300

    third commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,34 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollup = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is greater than 350 viewport height, add scroll to up button 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') 
+                        : scrollUp.classList.remove('show-scroll');
+}
 
-
+window.addEventListener('scroll', scrollup)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('.section[id]');
 
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            // Get the ID of the section
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link');
+            }else{
+                sectionsClass.classList.remove('active-link');
+            }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
